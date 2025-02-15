c449a00b7bf2a562dd17dc574d35af37bf17488e
Edu <udexeraco@hotmail.com>
2025-02-15 22:28:46 +0100

    222

---
+++ site/Práctica 4.1/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header class="header">
+        <div class="site-title">
+            <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+        </div>
+        <nav class="main-navigation">
+            <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+        </nav>
+    </header>
+    
